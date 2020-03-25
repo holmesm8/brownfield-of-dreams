64864d6b28c5404ea7c62ca5939e88ca004d983b
Ryan Allen <rcallen89@gmail.com>
2020-03-25 17:57:11 -0600

    General Bug fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
