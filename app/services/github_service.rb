2d9fdfbee474b3c83fee3356307143af96379808
Ryan Allen <rcallen89@gmail.com>
2020-03-25 11:24:24 -0600

    Refactory Github Services with Spec

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,10 +1,20 @@
 class GithubService
 
   def get_repos(user)
+    parsed = JSON.parse(api_response('/user/repos', user).body, symbolize_names: true)
+  end
+
+  def get_followers(user)
+    parsed = JSON.parse(api_response('/user/followers', user).body, symbolize_names: true)
+  end
+
+  private
+
+  def conn
     conn = Faraday.new('https://api.github.com')
+  end
 
-    response = conn.get('user/repos', {visibility: 'all'}, {'Accept' => 'application/json', :Authorization => user.github_token})
-
-    parsed = JSON.parse(response.body, symbolize_names: true)
+  def api_response(url, user) 
+    response = conn.get(url, nil, {:Authorization => user.github_token})
   end
 end
---
+++ spec/services/github_service_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Github Api Service Connection' do
+  before :each do
+    @user = User.create!(
+                      email: 'user1@example.com',
+                      first_name: 'Test',
+                      last_name: 'Face',
+                      password: 'password',
+                      role: 0,
+                      github_token: "#{ENV['GITHUB_API_KEY']}"
+                    )
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+
+  it 'can connect to the Github API Repos with creditionals and return array of hashes', :vcr => {record: :once} do
+    GitHub_API = GithubService.new
+
+    expect(GitHub_API.get_repos(@user).class).to eq(Array)
+    expect(GitHub_API.get_repos(@user)[0].class).to eq(Hash)
+  end
+
+  it 'can connect to the Github API Followers with creditionals and return array of hashes', :vcr => {record: :once} do
+    GitHub_API = GithubService.new
+
+    expect(GitHub_API.get_followers(@user).class).to eq(Array)
+    expect(GitHub_API.get_followers(@user)[0].class).to eq(Hash)
+  end
+end
