f3dd9aaecc7e1617852dd8a052cf45ea4e2b19e9
Ryan Allen <rcallen89@gmail.com>
2020-03-26 14:21:15 -0600

    Modify Schema for User Add Friendship Joins Table

---
+++ db/migrate/20200326172725_create_friendships.rb
@@ -0,0 +1,8 @@
+class CreateFriendships < ActiveRecord::Migration[5.2]
+  def change
+    create_table :friendships, :force => true, :id => false do |t|
+      t.bigint "user_id", :null => false
+      t.bigint "friend_id", :null => false
+    end
+  end
+end
