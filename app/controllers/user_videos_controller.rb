d44944bbd8dd778cd5d102ba918644e4fdf7518c
Ryan Allen <rcallen89@gmail.com>
2020-03-27 11:43:37 -0600

    Add Bookmark for logged in users function

--- app/controllers/user_videos_controller.rb
+++ app/controllers/user_videos_controller.rb
@@ -3,7 +3,12 @@ class UserVideosController < ApplicationController
   end
 
   def create
+    if current_user == nil
+      flash[:notice] = "Please Log In to Bookmark Videos"
+      return redirect_to login_path
+    end
     user_video = UserVideo.new(user_video_params)
+    user_video.user_id = current_user.id
     if current_user.user_videos.find_by(video_id: user_video.video_id)
       flash[:error] = "Already in your bookmarks"
     elsif user_video.save
@@ -16,6 +21,6 @@ class UserVideosController < ApplicationController
   private
 
   def user_video_params
-    params.permit(:user_id, :video_id)
+    params.permit(:video_id)
   end
 end
