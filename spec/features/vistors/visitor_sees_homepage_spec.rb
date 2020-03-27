625a68cca35c2220a7a1a2caef50b7d92ab2c5f4
Ryan Allen <rcallen89@gmail.com>
2020-03-27 11:44:06 -0600

    Add Hidden Tutorials to unregistered Users

--- spec/features/vistors/visitor_sees_homepage_spec.rb
+++ spec/features/vistors/visitor_sees_homepage_spec.rb
@@ -22,5 +22,50 @@ describe 'Visitor' do
         expect(page).to have_content(tutorial1.description)
       end
     end
+
+    it 'can see a list of tutorials that arent classroom content' do
+      tutorial1 = create(:tutorial)
+      tutorial2 = create(:tutorial, classroom: true)
+
+      video1 = create(:video, tutorial_id: tutorial1.id)
+      video2 = create(:video, tutorial_id: tutorial1.id)
+      video3 = create(:video, tutorial_id: tutorial2.id)
+      video4 = create(:video, tutorial_id: tutorial2.id)
+
+      visit root_path
+
+      expect(page).to have_css('.tutorial', count: 1)
+      within(first('.tutorials')) do
+        expect(page).to have_css('.tutorial')
+        expect(page).to have_css('.tutorial-description')
+        expect(page).to have_content(tutorial1.title)
+        expect(page).to have_content(tutorial1.description)
+      end
+    end
+
+    it 'a user can see things a visitor cant based on classroom content' do
+      user = create(:user)
+      tutorial1 = create(:tutorial)
+      tutorial2 = create(:tutorial, classroom: true)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+
+      video1 = create(:video, tutorial_id: tutorial1.id)
+      video2 = create(:video, tutorial_id: tutorial1.id)
+      video3 = create(:video, tutorial_id: tutorial2.id)
+      video4 = create(:video, tutorial_id: tutorial2.id)
+
+      visit root_path
+
+      expect(page).to have_css('.tutorial', count: 2)
+
+      within(first('.tutorials')) do
+        expect(page).to have_css('.tutorial')
+        expect(page).to have_css('.tutorial-description')
+        expect(page).to have_content(tutorial1.title)
+        expect(page).to have_content(tutorial1.description)
+      end
+    end
   end
 end
