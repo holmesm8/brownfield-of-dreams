6ccdabedff28091981d94a8e31639fbec64bccad
Ryan Allen <rcallen89@gmail.com>
2020-03-28 13:19:05 -0600

    Add Activation Email Template Mailer

---
+++ app/mailers/activation_mailer.rb
@@ -0,0 +1,12 @@
+class ActivationMailer < ApplicationMailer
+
+  def inform(user_params, user)
+    @key = user.set_confirmation_token
+    @user_name = "#{user_params[:first_name]} #{user_params[:last_name]}"
+    mail(to: user_params[:email], subject: "#{@user_name} please activate your account!")
+  end
+
+  def protect_against_forgery?
+    false
+  end
+end
